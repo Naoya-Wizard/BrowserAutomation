d2edd8f114f0bcae752097167ae12f8dc804760f
Naoya-Wizard <79356057+Naoya-Wizard@users.noreply.github.com>
2023-12-24 22:22:58 +0900

    Update テキストを指定して動作.py

--- テキストを指定して動作.py
+++ テキストを指定して動作.py
@@ -19,8 +19,28 @@ Raises:
 - Exception: 指定したタイムアウト時間内にエレメントを見つけることができなかった場合。
 """
 
-
 def click_element_with_exact_text(driver, text, timeout=60, scroll_amount=300):
+    start_time = time.time()
+    while True:
+        if time.time() - start_time > timeout:
+            raise Exception(f"Timeout on waiting for element with text '{text}'.")
+        try:
+            # normalize-space()を使用して空白を無視
+            element = driver.find_element(By.XPATH, f"//*[normalize-space(text()) = '{text}']")
+            element.click()
+            break
+        except NoSuchElementException:
+            # 要素が見つからない場合はスクロール
+            driver.execute_script(f"window.scrollBy(0, {scroll_amount});")
+        except ElementClickInterceptedException:
+            # 要素がクリック可能になるまで少し待つ
+            time.sleep(1)
+        except:
+            # その他の例外が発生した場合
+            continue
+
+
+def click_element_by_normalized_text(driver, text, timeout=60, scroll_amount=300):
     start_time = time.time()
     while True:
         if time.time() - start_time > timeout:
