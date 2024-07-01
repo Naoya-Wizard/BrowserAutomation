ad556463b96c352e9765b34a121835725592c24d
Naoya-Wizard <79356057+Naoya-Wizard@users.noreply.github.com>
2024-07-01 21:46:14 +0900

    Update テキストを指定して動作.py

--- テキストを指定して動作.py
+++ テキストを指定して動作.py
@@ -1,5 +1,6 @@
 import time
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, TimeoutException, ElementNotInteractableException
 
 
 """
